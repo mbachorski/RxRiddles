782f66f52a925aabae4aa73385151e098d4f338f
Marek Bachorski <m.bachorski@fibargroup.com>
2018-10-23 17:42:08 +0200

    adds solution for Riddle 22

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle22.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle22.kt
@@ -8,7 +8,6 @@ object Riddle22 {
      *
      * Use case: Group related data while skipping over some of it.
      */
-    fun solve(source: Observable<Int>): Observable<List<Int>> {
-        TODO()
-    }
+    fun solve(source: Observable<Int>): Observable<List<Int>> =
+            source.buffer(2, 3)
 }
